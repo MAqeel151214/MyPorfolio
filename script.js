9645b2f89431087901ec444b83e73c84ba79eb93
MAqeel151214 <aqeelkabirbalti@gmail.com>
2024-11-07 17:27:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Contact Form Submission
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('Thank you for reaching out! I will get back to you soon.');
+  this.reset();
+});
